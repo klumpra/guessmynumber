c3fb01f5483959eec0d68c713fdfea3ea7e55dbc
klumpra <klumpra@10.0.0.212>
2020-06-13 12:05:40 -0500

    First commit

---
+++ GuessMyNumber/src/edu/lewisu/cs/klump/practice/GuessMyNumberApp.java
@@ -0,0 +1,32 @@
+package edu.lewisu.cs.klump.practice;
+
+/* Ray Klump
+ * This program repeatedly asks the user to guess a randomly generated number between
+ * 1 and 10.
+ */
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessMyNumberApp {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int target, guess;
+		String keepGoing;
+		Random rnd = new Random();
+		do {
+			target = 1+rnd.nextInt(10);
+			do {
+				System.out.print("Enter your guess: ");
+				guess = sc.nextInt();
+				if (target == guess) {
+					System.out.println("Congratulations.");
+				} else {
+					System.out.println("Sorry. That is incorrect.");
+				}
+			} while (target != guess);
+			System.out.print("Go again (y or n)? ");
+			keepGoing = sc.next().toLowerCase().strip();
+		} while (keepGoing.equals("y"));
+		System.out.println("Thank you for playing.");
+	}
+}
